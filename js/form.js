b1b6ce592c0a9314d387c8b8a36af8f2b6ab8dab
sethbaldridge87 <sethbaldridge87@gmail.com>
2019-01-03 18:25:42 -0700

    Menu fixed

--- js/form.js
+++ js/form.js
@@ -1,6 +1,3 @@
-
-
-
 $(document).ready(function () {
     var date_input = $('input[name="date"]'); //our date input has the name "date"
     var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
